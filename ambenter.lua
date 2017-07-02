588b6980e06f1882517a940dd2d29c39693a45c8
JasonKnightt <maltese.rob@gmail.com>
2017-07-02 11:34:37 -0400

    Create ambenter.lua

---
+++ ambenter.lua
@@ -0,0 +1,32 @@
+-- Get In Ambulance --
+
+Citizen.CreateThread(function()
+	if IsControlJustPressed(1, 58) then
+	if IsPedInAnyVehicle(GetPlayerPed(-1), true) then
+			TriggerEvent('GetInAmbulance')
+		end
+	end
+end)
+
+
+AddEventHandler('GetInAmbulance', function()
+TriggerEvent('GetInAmbulance')
+	local victim = GetPlayerPed(-1)
+	local coordA = GetEntityCoords(playerped, 1)
+	local coordB = GetOffsetFromEntityInWorldCoords(playerped, 0.0, 5.0, 0.0)
+	local ambulance = getVehicleInDirection(coordA, coordB)
+	
+		if not IsPedInAnyVehicle(victim, true) then
+			local ambulance = getVehicleInDirection(4.0)
+			if GetEntityModel(ambulance) == GetHashKey("ambulance") then
+				TaskEnterVehicle(victim, ambulance, 6, 2, 1.0, 2, 0)
+			end
+		end
+end)
+
+
+function getVehicleInDirection(coordFrom, coordTo)
+	local rayHandle = CastRayPointToPoint(coordFrom.x, coordFrom.y, coordFrom.z, coordTo.x, coordTo.y, coordTo.z, 10, GetPlayerPed(-1), 0)
+	local a, b, c, d, vehicle = GetRaycastResult(rayHandle)
+	return vehicle
+end
